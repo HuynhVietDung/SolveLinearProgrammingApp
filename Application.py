12e43ef600b9a9618ddb577efda508f3e6b393c1
HuynhVietDung <122032587+HuynhVietDung@users.noreply.github.com>
2023-08-26 15:43:51 +0700

    Add  Application.py
    main file

---
+++ Application.py
@@ -0,0 +1,10 @@
+from gui import *
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.title("Solve Linear Programming Problem Application")
+    app.geometry('740x440')
+    #app.configure(bg = '#333333')
+    #app.resizable(False, False)
+    app.mainloop()
+
